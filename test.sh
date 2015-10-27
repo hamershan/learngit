cd7b396509b5a7bdb4e974fc3a8ca666ddad22b7
leo <wudashan@qq.com>
2015-10-27 13:21:35 +0800

    add new file

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X POST -H "Authorization: QBox lvQtCf2TgXV72enqjgLZDYBF6EeOCaouUcT1mkcG:NQjkdOcdv7x725TUtJ_HLvSVYaw=" --data "bucket=wdtopendiv&key=tt.jpg&fops=sequickimage/convert" "http://api.qiniu.com/pfop"
